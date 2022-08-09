e7a7eb5ad71c0630ae7a6e5a154b1c0b68112e72
Eworitse <williamseworitse@gmail.com>
2022-08-09 21:26:03 +0100

    get_precision.c created
    created file and added basic syntax

---
+++ get_precision.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * get_precision - calculates the precision for printing
+ * @format: formated string for printed
+ * @i: List of arguments to be printed.
+ * @list: list of arguments.
+ * Return: Precision.
+ */
+int get_precision(const char *format, int *i, va_list list)
+{
+    int curr_i = *i + 1;
+    int precision = -1;
+
+    if (format[curr_i] != '.')
+        return (precision);
+
+    precision = 0;
